3aa480a4df5ecaa17fdcc8990093ca41a7f80ca4
asifrauf1 <asifraufsi@gmail.com>
2020-06-12 12:59:47 +0500

    formatting changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (   
     <div className={`room ${brightness}`}>
     <h1>Room lights example with React state</h1>
-         The room is {isLit?"Lit":"dark"}
+         The room is <strong>{isLit?"Lit":"dark"}</strong>
         <br/>
         <button onClick = {() => setLit(!isLit)}>Flip Switch</button>
         <hr/>
@@ -19,7 +19,7 @@ function App() {
         <span> The room temperature is <strong>&lt;&lt; {roomTemp} &gt;&gt;</strong> </span>
         <button onClick ={() => setTemp(roomTemp - 1)}>-</button>
         <hr/>
-        <img height={300} widt src={isLit?"https://thumbs.dreamstime.com/b/modern-minimalist-sitting-room-interior-down-lights-grey-wall-above-upholstered-grey-sofa-ornamental-vases-40801051.jpghttps://thumbs.dreamstime.com/b/modern-minimalist-sitting-room-interior-down-lights-grey-wall-above-upholstered-grey-sofa-ornamental-vases-40801051.jpg":""}></img>
+        <img height={300} alt="Room Image" src={isLit?"https://thumbs.dreamstime.com/b/modern-minimalist-sitting-room-interior-down-lights-grey-wall-above-upholstered-grey-sofa-ornamental-vases-40801051.jpghttps://thumbs.dreamstime.com/b/modern-minimalist-sitting-room-interior-down-lights-grey-wall-above-upholstered-grey-sofa-ornamental-vases-40801051.jpg":""}></img>
         
     </div>    
   );
